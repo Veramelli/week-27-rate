c72737be58554730db361b5dc7f19b00205c5d37
VeraMelli <veralitv88@mail.ru>
2022-07-17 11:23:47 +0200

    Add change rate

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 import s from "./card.module.css";
 
 function Card(props) {
+  const [pressed, setPressed] = useState(false);
+
+  const handleChange = () => {
+    setPressed(!pressed);
+  };
+
   return (
-    <div className={s.card + (props.card.isSelected ? " " + s.selected : "")}>
+    <div className={s.card + (pressed ? " " + s.selected : "")}>
       <div
         className={s.title}
-        style={{ backgroundColor: props.card.titleColor }}>
+        style={{ backgroundColor: props.card.titleColor }}
+      >
         <div>{props.card.title}</div>
       </div>
-      <div className={s.wrapper} style={{ backgroundColor: props.card.priceColor }}>
-        
-          <div>руб</div>
-          <div className={s.price}> {props.card.price} </div>
-          <div>/мес</div>
-        
+      <div
+        className={s.wrapper}
+        style={{ backgroundColor: props.card.priceColor }}
+      >
+        <div>руб</div>
+        <div className={s.price}> {props.card.price} </div>
+        <div>/мес</div>
       </div>
       <div className={s.speed}>до{props.card.speed}Мбит/сек</div>
       <div className={s.footer}>
         <p className={s.footer_text}>{props.card.description}</p>
       </div>
+      <button onClick={handleChange} className={s.button}>
+        Выбрать тариф
+      </button>
     </div>
   );
 }
